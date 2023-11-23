b5b76fa23b266b844a44a97e28ed1e26f0d69b85
Helene Astaburuaga <127048528+Heleneastaburuaga@users.noreply.github.com>
2023-11-23 17:04:00 +0000

    aldatuta

--- routes/users.js
+++ routes/users.js
@@ -36,18 +36,27 @@ router.get('/list', function(req, res, next) {
 
 router.post("/new", (req, res) => {
   users.push(req.body);
-  db.bezeroak.insert(req.body, function(err,user)){
+  db.bezeroak.insert(req.body, function(err,user){
     if(err){
       console.log(err)
     }else{
       console.log(user)
       res.json(user);
     }
-  }
+  })
 });
 
 router.delete("/delete/:id", (req, res) => {
   users = users.filter(user => user.id != req.params.id);
+  //remove user form mongo
+  db.bezeroak.remove({_id: mongojs.ObjectId(req.params.id)} ,
+  function(err,user){
+    if(err){
+      console.log(err)
+    }else{
+      console.log(user)
+    }
+  })
   res.json(users);
 });
 
@@ -56,6 +65,16 @@ router.put("/update/:id", (req, res) => {
   user.izena = req.body.izena;
   user.abizena = req.body.abizena;
   user.email = req.body.email;
+  //updateuser in mongo
+  db.bezeroak.update({_id: mongojs.ObjectId(req.params.id)},
+    {$set: {izena: req.body.izena, abizena: req.body.abizena,email:req.body.email} },
+    function (err,user) {
+      if(err){
+        console.log(err)
+      } else {
+        console.log(user)
+      }
+    })
   res.json(users);
 })
 
